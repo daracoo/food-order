edac2c1964aacd5b7b5d652a8e8616060dab713c
daracoo <dkuzmanoski21@gmail.com>
2024-08-13 11:12:31 +0200

    Handling HTTP

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,6 +1,6 @@
 import MealItem from "./MealItem";
 import useHttp from "../hooks/useHttp";
-
+import Error from "./Error";
 const requestConfig = {};
 
 export default function Meals(){
@@ -9,9 +9,13 @@ export default function Meals(){
         error
     } = useHttp('http://localhost:3000/meals', requestConfig, []);
 
-    if(isLoading){
-        return <p>Fetching meals...</p>
-    }
+    if (isLoading) {
+        return <p className="center">Fetching meals...</p>;
+      }
+    
+      if (error) {
+        return <Error title="Failed to fetch meals" message={error} />;
+      }
     
     return <ul id="meals">{loadedMeals.map((meal) => (
         <MealItem key={meal.id} meal={meal} />
